e7098e10876af8a697caf2937bc0cb97356e4ee7
tkyoo <dbxorms4712@naver.com>
2022-03-30 15:17:07 +0900

    DataBase 설정정보 제외하여 구동되록 수정

--- src/main/java/tkyoo/project/shopping/ShoppingApplication.java
+++ src/main/java/tkyoo/project/shopping/ShoppingApplication.java
@@ -2,8 +2,10 @@ package tkyoo.project.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// exclude={DataSourceAutoConfiguration.class} => 당장은 사용하는 DB가 없으니 DB 설정정보를 제외
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ShoppingApplication {
 
 	public static void main(String[] args) {
