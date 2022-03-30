b0048a087b0df1754f2ed47b373be3bce42e1d02
tkyoo <dbxorms4712@naver.com>
2022-03-30 17:27:35 +0900

    mssql jdbc 의존성 주입 추가

--- src/main/java/tkyoo/project/shopping/ShoppingApplication.java
+++ src/main/java/tkyoo/project/shopping/ShoppingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// exclude={DataSourceAutoConfiguration.class} => 당장은 사용하는 DB가 없으니 DB 설정정보를 제외
+// (exclude={DataSourceAutoConfiguration.class}) => 당장은 사용하는 DB가 없으니 DB 설정정보를 제외
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ShoppingApplication {
 
